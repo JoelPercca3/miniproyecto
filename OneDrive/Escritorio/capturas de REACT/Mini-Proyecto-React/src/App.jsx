53907c96d6ccbb4c9d752df02c5ca50019ba5f90
Joel Percca <joelpercca3@gmail.com>
2024-04-21 22:37:21 -0500

    first copmmit

---
+++ OneDrive/Escritorio/capturas de REACT/Mini-Proyecto-React/src/App.jsx
@@ -0,0 +1,24 @@
+import Main from "./Main/Main";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./components/Footer";
+import { Fragment } from "react";
+import Modal from "./components/Modal";
+import { useSelector } from "react-redux";
+
+function App() {
+  return (
+    <div className="xl:container xl:mx-auto md:px-8 px-5">
+      <ModalComponent />
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+function ModalComponent() {
+  const modalState = useSelector((state) => state.modal.showModal);
+  return <Fragment>{modalState && <Modal />}</Fragment>;
+}
+
+export default App;
